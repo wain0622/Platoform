71086f8277818c8b5a211b66ea7ccfe3d05c73af
wain0622 <83865756+wain0622@users.noreply.github.com>
2021-09-04 18:30:49 +0900

    敵2体完成

---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class EnemyScript : MonoBehaviour
+{
+
+    int counter = 0;
+    float move = 0.05f;
+
+    void Update()
+    {
+        Vector3 p = new Vector3(move, 0, 0);
+        transform.Translate(p);
+        counter++;
+
+        //countが100になれば-1を掛けて逆方向に動かす
+        if (counter == 100)
+        {
+            counter = 0;
+            move *= -1;
+        }
+    }
+}
