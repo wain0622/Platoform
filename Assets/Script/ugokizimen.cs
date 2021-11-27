fd97f5e67ef393bf516d78601d274a929d629641
wain0622 <83865756+wain0622@users.noreply.github.com>
2021-11-27 19:19:25 +0900

    すてーじ 3開発開始

--- Assets/Script/ugokizimen.cs
+++ Assets/Script/ugokizimen.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ugokizimen : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public int direction = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class ugokizimen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3 (1, 0, 0);
+        if (transform.position.x >= -13)
+        {
+            direction = -1;
+        }
+        else if (transform.position.x <= -18)
+        {
+            direction = 1;
+        }
+
+        transform.position += new Vector3 (speed*direction, 0, 0);
+
+
     }
+
 }
