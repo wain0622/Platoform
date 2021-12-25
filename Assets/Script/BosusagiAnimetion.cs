defcb6169db3df0cbd53e4d8a2f94b44a17f408f
wain0622 <83865756+wain0622@users.noreply.github.com>
2021-12-25 19:34:46 +0900

    うさぎの仕様,攻撃範囲などを設定した

---
+++ Assets/Script/BosusagiAnimetion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BosusagiAnimetion : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
