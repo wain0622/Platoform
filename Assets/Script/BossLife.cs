e30ac8c22821a67c65ca85d1b7fcfccd32c6b2a6
wain0622 <83865756+wain0622@users.noreply.github.com>
2022-04-23 19:29:41 +0900

    ボスの体力かんせい？

---
+++ Assets/Script/BossLife.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLife : MonoBehaviour
+{
+    public int bossLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossLife = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bossLife <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Beam")
+        {
+            bossLife -= 75;
+
+        }
+    }
+}
---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = Player.transform.position.x;
+        float posy = Player.transform.position.y;
+        float posz = Player.transform.position.z;
+
+        transform.position = new Vector3(posx, posy + 2, posz + 16);
+    }
+}
