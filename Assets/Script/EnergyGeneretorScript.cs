f702a2ebfb2b198c8c9b791f0175f53ca61a2743
wain0622 <83865756+wain0622@users.noreply.github.com>
2022-09-10 19:33:15 +0900

    エナジー進化させた

---
+++ Assets/Script/EnergyGeneretorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyGeneretorScript : MonoBehaviour
+{
+    public int energys;
+    public GameObject energy;
+    private Vector3 energyPos;
+    public int energyQuantity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        energyPos = this.transform.position;
+        energyQuantity = 10;//ビーム1回打つのに必要な数(プレイヤースクリプトで使用しているから消しちゃダメ)
+        for(int i= 0; i < 3; i++)
+        {
+            energyPos.x = Random.Range(-28, 4);
+            Instantiate(energy, energyPos, Quaternion.identity);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        energys = GameObject.FindGameObjectsWithTag("Energy").Length;
+
+        if (energys <= 2)
+        {
+            energyPos.x = Random.Range(-28, 4);
+            Instantiate(energy, energyPos, Quaternion.identity);
+        }
+    }
+}
